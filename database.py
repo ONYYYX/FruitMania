5dbbcf9ca744c7b36bb6c5702fb005edca947161
ONYYYX <onyxservers@yandex.ru>
2020-01-15 00:16:07 +0400

    Added support for different modes for score table

--- database.py
+++ database.py
@@ -7,11 +7,14 @@ class Database:
         self.connection: sqlite3.Connection = sqlite3.connect(config.database)
         self.cursor: sqlite3.Cursor = self.connection.cursor()
 
-    def get_best_score(self) -> int:
-        return self.cursor.execute('SELECT MAX(score) FROM scores').fetchone()[0]
+    def get_best_score(self, mode) -> int:
+        score = self.cursor.execute('SELECT MAX(score) FROM scores WHERE mode=?', (mode, )).fetchone()[0]
+        if score is None:
+            score = 0
+        return score
 
-    def add_score(self, score) -> None:
-        self.cursor.execute('INSERT INTO scores (score) VALUES (?)', (score, ))
+    def add_score(self, score, mode) -> None:
+        self.cursor.execute('INSERT INTO scores (score, mode) VALUES (?, ?)', (score, mode))
         self.connection.commit()
 
     def close(self) -> None:
